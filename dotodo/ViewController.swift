3386764ac9d03c9a937385936ba5fc04173530a2
Mohammad Al-Ahdal <malahdal@ualberta.ca>
2018-01-22 09:40:17 -0700

    Initial Commit

---
+++ dotodo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dotodo
+//
+//  Created by Mohammad Al-Ahdal on 2018-01-22.
+//  Copyright © 2018 Mohammad Al-Ahdal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
