b9165763fb704fc9d1ba3a2d5e64016b251b11ed
RoyalKingMomo <malahdal@ualberta.ca>
2018-04-12 00:33:31 -0600

    add most files I'll need

---
+++ dotodo/CalendarScreen.swift
@@ -0,0 +1,9 @@
+//
+//  CalendarScreen.swift
+//  dotodo
+//
+//  Created by Mohammad Al-Ahdal on 2018-04-12.
+//  Copyright © 2018 Mohammad Al-Ahdal. All rights reserved.
+//
+
+import Foundation
---
+++ dotodo/CustomRootTabViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CustomRootTabViewController.swift
+//  dotodo
+//
+//  Created by Mohammad Al-Ahdal on 2018-04-12.
+//  Copyright © 2018 Mohammad Al-Ahdal. All rights reserved.
+//
+
+import UIKit
+
+class CustomRootTabController: UIViewController {
+    
+    // K.I.S.S.
+    // we're going to have 3 views
+    // a calendar view
+    // an add task view with current tasks
+    // a settings view
+    
+    override func viewDidLoad() {
+        super.viewDidLoad();
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        // add code
+    }
+    
+    
+    
+}
