b3fd091b52ee9ca50c72d842d477b6397c4f99e7
RoyalKingMomo <mohammad@enggmomo.me>
2018-04-14 23:14:03 -0600

    actually, I wanna make this custom. Removing IGListKit

--- dotodo/TasksScreen.swift
+++ dotodo/TasksScreen.swift
@@ -9,48 +9,13 @@
 import UIKit
 import IGListKit
 
-class TasksScreen : UIViewController, ListAdapterDataSource {
+class TasksScreen : UIViewController {
     
     var data : [Any] = ["hi", "eh"]
     
-    
-    func objects(for listAdapter: ListAdapter) -> [ListDiffable] {
-        return data as! [ListDiffable];
-    }
-    
-    func listAdapter(_ listAdapter: ListAdapter, sectionControllerFor object: Any) -> ListSectionController {
-        
-        
-        
-        return TasksSectionController();
-    }
-    
-    func emptyView(for listAdapter: ListAdapter) -> UIView? {
-        let redView = UIView()
-        redView.frame = self.view.frame
-        redView.backgroundColor = UIColor.red;
-        
-        return redView
-    }
-    
-    
-    
-    lazy var adapter : ListAdapter = {
-        return ListAdapter(updater: ListAdapterUpdater(), viewController: self);
-    }()
-    var colletionView:UICollectionView!;
-    
     override func viewDidAppear(_ animated: Bool) {
         self.view.backgroundColor = UIColor.cyan;
-        colletionView = UICollectionView(frame: self.view.frame, collectionViewLayout: UICollectionViewLayout.init());
-        self.colletionView.backgroundColor = UIColor.brown;
-        adapter.collectionView = colletionView;
-        adapter.dataSource = self;
         
-        self.view.addSubview(colletionView);
     }
 }
 
-class TasksSectionController: ListSectionController {
-    
-}
